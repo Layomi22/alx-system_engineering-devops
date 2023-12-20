2a18f8047b89035bf2d5218f51f4c103b154a519
Layomi22 <sewejlayomi18@gmail.com>
2023-12-19 20:06:27 -0800

    tasks done

---
+++ 0x14-mysql/create_db_table.sql
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+-- Create a database and a table with one entry server                                     
+
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6 (id,name) VARCHAR(20);
+INSERT INTO nexus6 (id,name) VALUES(1,"Leon");
+GRANT SELECT ON nexus6 TO 'holberton_user'@'localhost'
